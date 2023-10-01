1283a8b0297f653df935cdc6bdb3f514a8517eed
mariamartinez44 <mc.martinezm1@uniandez.edu.co>
2023-10-01 16:50:11 -0500

    otros cambios

--- src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
+++ src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
@@ -12,11 +12,16 @@ import uniandes.edu.co.proyecto.modelo.Usuarios;
 import uniandes.edu.co.proyecto.modelo.tipoHabitaciones;
 import uniandes.edu.co.proyecto.repositorio.UsuariosRepository;
 import uniandes.edu.co.proyecto.repositorio.tipoHabitacionRepository;
+import uniandes.edu.co.proyecto.repositorio.ReservasRepository;
+import uniandes.edu.co.proyecto.repositorio.cuentaConsumosRepository;
+
+import uniandes.edu.co.proyecto.repositorio.
+
 
 @SpringBootApplication
 public class ProyectoApplication implements CommandLineRunner{
 	@Autowired
-	private tipoHabitacionRepository usuariosRepository;
+	private ReservasRepository ReservasRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
 	}
@@ -24,8 +29,8 @@ public class ProyectoApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) {
 		// TODO Auto-generated method stub
-		Collection<tipoHabitaciones> usuarios = usuariosRepository.darTipoHabitaciones();
-		for (tipoHabitaciones b:usuarios){
+		Collection<tipoHabitaciones> Reservas = ReservasRepository.darTipoHabitaciones();
+		for (Reservas b:Reservas){
 			System.out.println(b);
 		}
 		
