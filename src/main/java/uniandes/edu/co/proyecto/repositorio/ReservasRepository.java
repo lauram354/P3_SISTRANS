c9e6082087d649e36d8223fa9461fced949b058c
mariamartinez44 <mc.martinezm1@uniandez.edu.co>
2023-10-01 17:25:17 -0500

    cambios

--- src/main/java/uniandes/edu/co/proyecto/repositorio/ReservasRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/ReservasRepository.java
@@ -1,5 +1,48 @@
 package uniandes.edu.co.proyecto.repositorio;
 
-public interface ReservasRepository {
-    
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import jakarta.transaction.Transactional;
+import uniandes.edu.co.proyecto.modelo.Reservas;
+
+public interface ReservasRepository extends JpaRepository<Reservas, Integer>{
+
+    //RF9 hace falta acualizar llegada al hotel
+    //Toca colocar que estas operaciones solo recepcionista
+
+    //Consultar la reserva hotel. Si check_in está en false, no ha llegado. Si check_in true y check_out está en false, no se ha ido.
+    //Si check_in y check_ou en false, el cliente no ha llegado al hotel.
+    @Query(value = "SELECT idReserva, numPersonas, fechaInicial, fechaFinal, id, tipo,  Habitaciones_id, check_in, check_out SELECT FROM Reservas WHERE idReserva= : idReserva", nativeQuery = true)
+    Collection<Reservas> darReserva(@Param("idReserva") Integer idReserva);
+
+    //Registrar llegada al hotel. 
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Reservas SET check_in = true WHERE idReserva= :idReserva", nativeQuery = true)
+    void registrar_Check_in_Reserva(@Param("idReserva") Integer idReserva);
+
+    //Borrar llegada al hotel.
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Reservas SET check_in = false WHERE idReserva= :idReserva", nativeQuery = true)
+    void borar_Check_in_Reserva(@Param("idReserva") Integer idReserva);
+
+    //RF11 hace falta acualizar salida al hotel 
+
+    //Registrar salida del hotel. 
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Reservas SET check_out = true WHERE idReserva= :idReserva", nativeQuery = true)
+    void registrar_Check_out_Reserva(@Param("idReserva") Integer idReserva);
+
+    //Borrar salida del hotel.
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Reservas SET check_out = false WHERE idReserva= :idReserva", nativeQuery = true)
+    void borar_Check_out_Reserva(@Param("idReserva") Integer idReserva);
+
 }
--- src/main/java/uniandes/edu/co/proyecto/repositorio/cuentaConsumoRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/cuentaConsumoRepository.java
@@ -1,5 +1,25 @@
+
 package uniandes.edu.co.proyecto.repositorio;
 
-public interface cuentaConsumoRepository {
-    
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import jakarta.transaction.Transactional;
+import uniandes.edu.co.proyecto.modelo.cuentaConsumos;
+
+public interface cuentaConsumosRepository extends JpaRepository<cuentaConsumos, Integer>{
+
+    //RF10 
+    //Toca colocar que solo empleado
+    //
+    //
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO cuentaConsumos(pagado, idReserva, fecha) VALUES (:pagado, :(SELECT idReserva, numPersonas, fechaInicial, fechaFinal, Habitaciones_id, tipo, Usuarios_idUsuario, check_in, check_out FROM Reservas WHERE Habitaciones_id= : 
+    Habitaciones_id), :fecha)", nativeQuery = true)
+    void insertarConsumo(@Param("pagado") boolean pagado, @Param("Habitaciones_id") String nombreUsuario, @Param("email") String email, @Param("rol") String rol, @Param("Hoteles_nombreHotel") String Hoteles_nombreHotel, @Param("tipoId") String tipoId);
+
 }
