767c2e4ee0bb0e8567c848a77db064d7976df542
lauram354 <l.murciac@uniandes.edu.co>
2023-10-01 13:49:30 -0500

    v

--- src/main/java/uniandes/edu/co/proyecto/repositorio/serviciosHotelRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositorio/serviciosHotelRepository.java
@@ -6,28 +6,29 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import jakarta.persistence.Tuple;
 import jakarta.transaction.Transactional;
-import uniandes.edu.co.proyecto.modelo.Habitaciones;
-import uniandes.edu.co.proyecto.modelo.tipoHabitaciones;
+import uniandes.edu.co.proyecto.modelo.serviciosHotel;
 
-public interface habitacionesRepository extends JpaRepository<Habitaciones, Integer>{
+public interface serviciosHotelRepository extends JpaRepository<serviciosHotel, Tuple>{
 
-    @Query(value = "SELECT * FROM Habitaciones", nativeQuery=true)
-    Collection<tipoHabitaciones> darHabitaciones();
+    @Query(value = "SELECT * FROM servHotel", nativeQuery=true)
+    Collection<serviciosHotel> darServicios();
 
-    @Query(value = "SELECT * FROM Habitaciones WHERE id= :id", nativeQuery = true)
-    tipoHabitaciones darHabitacion(@Param("id") Integer id);
+    @Query(value = "SELECT * FROM servHotel WHERE Hoteles_nombreHotel = Hoteles_nombreHotel;", nativeQuery = true)
+    serviciosHotel darHabitacion(@Param("Hoteles_nombreHotel") Integer Hoteles_nombreHotel);
 
-    
+    /**
     @Modifying
     @Transactional
-    @Query(value = "INSERT INTO Habitaciones(id, tv, minibar, cafetera, tipoHabi_tipo, Hoteles_nombreHotel) VALUES (Habitaciones_id_seq.nextval,:tv,:minibar,:cafetera, :tipoHabi_tipo, :Hoteles_nombreHotel)", nativeQuery = true)
+    @Query(value = "INSERT INTO Habitaciones(id, tv, minibar, cafetera, tipoHabi_tipo, Hoteles_nombreHotel) VALUES (Habitaciones_id_seq.nextval,:tv,:minibar,:cafetera, :tipoHabi_tipo, Hoteles_nombreHotel)", nativeQuery = true)
     void insertarHabitacion( @Param("tv") Boolean tv, @Param("minibar") Boolean minibar, @Param("cafetera") Boolean cafetera,
      @Param("tipoHabi_tipo") String tipoHabi_tipo, @Param("Hoteles_nombreHotel") String Hoteles_nombreHotel);
     
     @Modifying
     @Transactional
-    @Query(value = "UPDATE Habitaciones SET tv = :tv, minibar = :minibar, cafetera = :cafetera, tipoHabi_tipo =  :tipoHabi_tipo, Hoteles_nombreHotel = :Hoteles_nombreHotel  WHERE id =:id;", nativeQuery = true)
+    @Query(value = "UPDATE Habitaciones SET tv = :tv, minibar = :minibar, cafetera = :cafetera, tipoHabi_tipo =  tipoHabi_tipo, Hoteles_nombreHotel = :Hoteles_nombreHotel  WHERE id =:id;", nativeQuery = true)
     void actualizarHabitacion(@Param("id") Integer id, @Param("tv") Boolean tv, @Param("minibar") Boolean minibar, @Param("cafetera") Boolean cafetera,
      @Param("tipoHabi_tipo") String tipoHabi_tipo, @Param("Hoteles_nombreHotel") String Hoteles_nombreHotel);
 
@@ -35,5 +36,6 @@ public interface habitacionesRepository extends JpaRepository<Habitaciones, Inte
     @Transactional
     @Query(value = "DELETE FROM Habitaciones WHERE id = :id", nativeQuery = true)
     void eliminarTipoHabitacion(@Param("id") Integer id);
+    */
 
 }
